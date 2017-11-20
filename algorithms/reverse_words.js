1a6949c5b11d08b5c811625858d84fa22d1b21c0
Lal Zada <lalzadamohmand@gmail.com>
2017-11-20 18:33:12 +0500

    Create reverse_words.js

---
+++ algorithms/reverse_words.js
@@ -0,0 +1,39 @@
+/*
+
+    Reverse each word of string and return new string
+    Don't reverse the whole string just each word separately.
+    Don't use array reverse() function
+
+*/
+
+function reverseWords(str){
+    
+    let strArr = str.split(" ");
+    
+    for (let i=0; i < strArr.length; i++) {
+        
+        const word = strArr[i];
+        let reverseWord = "";
+        
+        for ( let j=word.length - 1; j >= 0; j--) {
+            reverseWord += word[j];
+        }
+        
+        /*
+            OR 
+            
+            for ( let j=0; j < word.length; j++) {
+                reverseWord += word[word.length - (j+1)];
+            }
+        */
+        
+        
+        strArr[i] = reverseWord;
+        
+    };
+    
+    return strArr.join(" ");
+}
+
+
+reverseWords('i am here');
