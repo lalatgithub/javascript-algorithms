6cbab6741cc9208c77f7fc3be4d63cc2bb8c8628
Lal Zada <lalzadamohmand@gmail.com>
2017-11-20 22:39:53 +0500

    Create two_sum.js

---
+++ algorithms/two_sum.js
@@ -0,0 +1,29 @@
+/*
+
+    Return all pairs of elements which is equal to the provided sum
+
+*/
+
+
+function twoSum(arr, sum){
+    
+    let pairs = [];
+    
+    arr.forEach(element => {
+        
+        // counterPart = the number which makes the current element equal to the provided sum
+        const counterPart = sum - element; 
+        
+        // if counterPart exist in array, it means pair could be made which is equal to provided sum
+        if (arr.indexOf(counterPart) !== -1) pairs.push([element, counterPart]);
+        
+    });
+    
+    return pairs;
+    
+}
+
+twoSum([1, 5, 3, 8, 6, 12, 9, 4], 7);
+
+
+
