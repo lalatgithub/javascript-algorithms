fa71c76aee37874c33f19930b75fc4671723aedf
Lal Zada <lalzadamohmand@gmail.com>
2017-11-21 22:29:14 +0500

    Create binary_search.js

---
+++ algorithms/binary_search.js
@@ -0,0 +1,31 @@
+ /*
+
+    Search a give key from a provided array.
+    Use recursion to avoid time complexity of O(n) 
+
+*/
+
+
+function binarySearch(arr, key){
+    
+    const middleIndex = Math.floor(arr.length / 2);
+    const middleElement = arr[middleIndex];
+    
+    if (key === middleElement) return true;
+    
+    else if (key < middleElement && arr.length > 1){
+        return binarySearch(arr.splice(0, middleIndex), key);
+    }
+    
+    else if (key > middleElement && arr.length > 1){
+        return binarySearch(arr.splice(middleIndex, arr.length - middleIndex), key);
+    }
+    
+    else return false;
+
+}
+
+const arr = [11,12,13,14,15,16];
+binarySearch(arr, 15);
+
+
