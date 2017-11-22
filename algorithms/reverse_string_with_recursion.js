4005f01767c09ff1202bf3f2c6995f82d87f06b5
Lal Zada <lalzadamohmand@gmail.com>
2017-11-22 17:13:14 +0500

    Update reverse_string_with_recursion.js

--- algorithms/reverse_string_with_recursion.js
+++ algorithms/reverse_string_with_recursion.js
@@ -9,7 +9,10 @@ function reverseString(str){
  
     if (str.length <= 1) return str;
     
-    return str.slice(str.length - 1, str.length) + reverseString(str.slice(0, str.length - 1));
+    const lastChar = str.slice(str.length - 1, str.length);
+    const remainingStr = str.slice(0, str.length - 1)
+    
+    return lastChar + reverseString(remainingStr);
     
 }
 
